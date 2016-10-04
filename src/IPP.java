7b3401066390908c6a294451b279609653395317
Dorina <dorina.doering@yahoo.de>
2016-10-04 16:11:46 +0200

    i am super happy thad we fixed that problem!!

---
+++ src/IPP.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Nelson on 19.09.2016.
+ */
+public class IPP extends Thread {
+    public static final int N = 5;
+    private IPP ipp;
+
+    public IPP(IPP i) {
+        ipp = i;
+    }
+
+    
+
+
+}
---
+++ src/javaTest.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Nelson on 25.09.2016.
+ */
+public class javaTest {
+}
