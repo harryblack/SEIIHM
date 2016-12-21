81840d0c2ca176fad246642b4af60d98198af040
Sascha Siemens <ssiemens@hm.edu>
2016-12-21 15:59:11 +0100

    Dateien für Aufgabe 7 angelegt

--- ssiemens/ss16/netzwerke/uebung7_filetransfer/FileReceiver.java
+++ ssiemens/ss16/netzwerke/uebung7_filetransfer/FileReceiver.java
@@ -1,7 +1,13 @@
 package ssiemens.ss16.netzwerke.uebung7_filetransfer;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+
 /**
  * Created by Sascha on 20/12/2016.
  */
 public class FileReceiver {
+
 }
--- ssiemens/ss16/netzwerke/uebung7_filetransfer/FileSender.java
+++ ssiemens/ss16/netzwerke/uebung7_filetransfer/FileSender.java
@@ -3,5 +3,5 @@ package ssiemens.ss16.netzwerke.uebung7_filetransfer;
 /**
  * Created by Sascha on 20/12/2016.
  */
-public class FileTransmitter {
+public class FileSender {
 }
